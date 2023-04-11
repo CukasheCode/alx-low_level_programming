06017611d8c85152282eaaa86502e3d5502154ab
CukasheCode <mismail.skudai@gmail.com>
2023-04-11 05:06:54 -0700

    update

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,16 @@
-int _strlen(char *s);
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: returns length as integer;
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
